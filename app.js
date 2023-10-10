7de6b7969fca44c4ea98eabdba6a275d00edeee9
JershonEspitia <jershonespitiarey@gmail.com>
2023-10-10 13:26:35 -0500

    feat: :sparkles: Add button more fields

--- app.js
+++ app.js
@@ -1,160 +1,198 @@
-import { postAll, getAll, getOne, deleteOne, putOne } from "./storage/methods.js"
-import { attributes, endPoint } from "./storage/profile.js"
+import {
+  postAll,
+  getAll,
+  getOne,
+  deleteOne,
+  putOne,
+} from "./storage/methods.js";
+import { attributes, endPoint } from "./storage/profile.js";
 
-addEventListener("DOMContentLoaded", ()=>{
-    functFormulario();
+addEventListener("DOMContentLoaded", () => {
+
+  let btnRegistrar = document.querySelector("#btnRegistrar");
+  let contenidoMain = document.querySelector("#contenidoMain");
+  let migaPan = document.querySelector("#migaPan");
+
+  btnRegistrar.addEventListener("click", () => {
+    functFormulario({contenidoMain, migaPan});
+    addCampo();
+  });
+  
 });
 
+let functFormulario = ({contenidoMain, migaPan}) => {
 
-let functFormulario = () => {
+  migaPan.textContent = "Registrar";
+  contenidoMain.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card">
+              <form>
+              <fieldset>
+                <legend>Registrar Portafolio</legend> 
 
-    let btnRegistrar = document.querySelector("#btnRegistrar");
-    let contenidoMain = document.querySelector("#contenidoMain");
-    let migaPan = document.querySelector("#migaPan");
+                <!--NOMBRE -->
+                <div>
+                  <label>Nombre</label>
+                  <input type="text" id="name" required>
+                </div>
 
-    btnRegistrar.addEventListener("click", ()=>{
-        migaPan.textContent = "Registrar"
-        contenidoMain.insertAdjacentHTML(
-            "beforeend",
-            `<form>
-            <fieldset>
-              <legend>Registrar Portafolio</legend> 
-              <!--NOMBRE -->
-              <div>
-                <label>Nombre</label>
-                <input type="text" id="name">
-              </div>
-              <!--CEDULA -->
-              <div>
-                <label>Cedula</label>
-                <input type="number" id="cedula">
-              </div>
-              <!-- FECHA NACIMIENTO -->
-              <div>
-                <label>Fecha Nacimiento</label>
-                <input type="date" id="fechaNacimiento">
-              </div>
-              <!--CIUDAD -->
-              <div>
-                <label>Ciudad</label>
-                <input type="text" id="ciudad">
-              </div>
-              <!-- TELEFONO -->
-              <div>
-                <label>Telefono</label>
-                <input type="tel" name="telefonos"></input>
-              </div>
-              <!-- Button -->
-              <button id="addTelefono">Agregar otro telefono</button>
-              <!-- Email -->
-              <div>
-                <label>Correo</label>
-                <input type="email" name="correos">
-              </div>
-              <!-- Button -->
-              <button id="addCorreo">Agregar otro correo</button>
+                <!--CEDULA -->
+                <div>
+                  <label>Cedula</label>
+                  <input type="number" id="cedula" required>
+                </div>
+                
+                <!-- FECHA NACIMIENTO -->
+                <div>
+                  <label>Fecha Nacimiento</label>
+                  <input type="date" id="fechaNacimiento" required>
+                </div>
 
+                <!--CIUDAD -->
+                <div>
+                  <label>Ciudad</label>
+                  <input type="text" id="ciudad" required>
+                </div>
 
+                <!-- TELEFONOS -->
+                <div>
+                  <label>Telefono</label>
+                  <input type="tel" name="telefonos" required></input>
+                </div>
+                <!-- Button -->
+                <input id="btnTelefono" class="favorite styled" type="button" value="(+) Telefono" />
 
+                <!-- CORREO -->
+                <div>
+                  <label>Correo</label>
+                  <input type="email" name="correos" required>
+                </div>
+                <!-- Button -->
+                <input id="btnCorreo" class="favorite styled" type="button" value="(+) Email" />
 
+                <!--SOCIALS -->
+                <div>
+                  <label>Redes Sociales</label>
+                  <input type="url" name="socials" placeholder="Link red social" required>
+                </div>
+                <!-- Button -->
+                <input id="btnSocial" class="favorite styled" type="button" value="(+) Social" />
 
-              <!-- Number -->
-              <div>
-                <label for="number">number</label>
-                <input type="number" id="number">
-              </div>
-              <!-- Password -->
-              <div>
-                <label for="password">password</label>
-                <input type="password" id="password">
-              </div>
-              <!-- Time -->
-              <div>
-                <label for="time">time</label>
-                <input type="time" id="time">
-              </div>
-              <!-- Checkbox -->
-              <div>
-                <label for="checkbox">checkbox</label>
-                <input type="checkbox" id="checkbox">
-              </div>
-              <!-- Date -->
-              <div>
-                <label for="date">date</label>
-                <input type="date" id="date">
-              </div>
-              <!-- Url -->
-              <div>
-                <label for="url">url</label>
-                <input type="url" id="url">
-              </div>
-              <!-- Radio -->
-              <div>
-                <label for="radio">radio</label>
-                <input type="radio" id="radio">
-              </div>
-              <!-- TextArea -->
-              <div>
-                <label for="textarea">textarea</label>
-                <textarea type="textarea" id="textarea"></textarea>
-              </div>
-              <!-- Select -->
-              <div>
-                <label for="select">select</label>
-                <select id="select">
-                  <option value="opt1">opt1</option>
-                  <option value="opt2">opt2</option>
-                  <option value="opt3">opt3</option>
-                </select>
-              </div>
-              
-              <!-- NEW types - not supported by older web browsers -->
-              
-              <!-- Color -->
-              <div>
-                <label for="color">color</label>
-                <input type="color" id="color"></input>
-              </div>
-              <!-- Datetime-local -->
-              <div>
-                <label for="datetime-local">datetime-local</label>
-                <input type="datetime-local" id="datetime-local"></input>
-              </div>
-              <!-- Month -->
-              <div>
-                <label for="month">month</label>
-                <input type="month" id="month"></input>
-              </div>
-              <!-- Range -->
-              <div>
-                <label for="range">range</label>
-                <input type="range" id="range"></input>
-              </div>
-              <!-- Search -->
-              <div>
-                <label for="search">search</label>
-                <input type="search" id="search"></input>
-              </div>
-              <!-- Tel -->
-              <div>
-                <label for="tel">tel</label>
-                <input type="tel" id="tel"></input>
-              </div>
-              <!-- Week -->
-              <div>
-                <label for="week">week</label>
-                <input type="week" id="week"></input>
-              </div>
-              
-              <!-- Button -->
-              <button>Submit</button>
-            </fieldset>
-          </form>`
-        );
-    });
-}; 
+                <!--ABOUT -->
+                <div>
+                  <label>Sobre mi</label>
+                  <textarea id="about" rows="4" cols="50" required></textarea>
+                </div>
 
-{/* <div class="data">
+                <!--HOBBIES -->
+                <div>
+                  <label>Hobbie</label>
+                  <input type="text" name="hobbies" required>
+                </div>
+                <!-- Button -->
+                <input id="btnHobbie" class="favorite styled" type="button" value="(+) Hobbie" />
+
+                <!--EXPERIENCES -->
+                <div>
+                  <label>Experiencia</label>
+                  <input type="text" name="experiences" required>
+                </div>
+                <!-- Button -->
+                <input id="btnExperience" class="favorite styled" type="button" value="(+) Experiencia" />
+
+                <!--SKILLS -->
+                <div>
+                  <label>Skill</label>
+                  <input type="text" name="skills" required>
+                </div>
+                <!-- Button -->
+                <input id="btnSkill" class="favorite styled" type="button" value="(+) Skill" />
+
+                <!--IDIOMA -->
+                <div>
+                  <label>Idioma</label>
+                  <input type="text" name="idiomas" required>
+                </div>
+                <!-- Button -->
+                <input id="btnIdioma" class="favorite styled" type="button" value="(+) Idioma" />
+                
+                <!-- Button -->
+                <button>REGISTRAR</button>
+              </fieldset>
+            </form>
+          </div>`
+    );
+};
+
+let addCampo = () => {
+  let btnTelefono = document.querySelector("#btnTelefono")
+  let btnCorreo = document.querySelector("#btnCorreo")
+  let btnSocial = document.querySelector("#btnSocial")
+  let btnHobbie = document.querySelector("#btnHobbie")
+  let btnExperience = document.querySelector("#btnExperience")
+  let btnSkill = document.querySelector("#btnSkill")
+  let btnIdioma = document.querySelector("#btnIdioma")
+
+  btnTelefono.addEventListener("click", () => {
+    btnTelefono.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+          <input type="tel" name="telefonos" required></input>
+        </div>`
+    );
+  });
+  btnCorreo.addEventListener("click", () => {
+    btnCorreo.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+          <input type="email" name="correos" required>
+        </div>`
+    );
+  });
+  btnSocial.addEventListener("click", () => {
+    btnSocial.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+          <input type="url" name="socials" placeholder="Link red social" required>
+        </div>`
+    );
+  });
+  btnHobbie.addEventListener("click", () => {
+    btnHobbie.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+          <input type="text" name="hobbies" required>
+        </div>`
+    );
+  });
+  btnExperience.addEventListener("click", () => {
+    btnExperience.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+          <input type="text" name="experiences" required>
+        </div>`
+    );
+  });
+  btnSkill.addEventListener("click", () => {
+    btnSkill.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+          <input type="text" name="skills" required>
+        </div>`
+    );
+  });
+  btnIdioma.addEventListener("click", () => {
+    btnIdioma.insertAdjacentHTML(
+      "beforebegin",
+      `<div>
+            <input type="text" name="idiomas" required>
+        </div>`
+    );
+  });
+};
+
+{
+  /* <div class="data">
     <div class="content-data">
         <div class="head">
             <h3>Sales Report</h3>
@@ -176,4 +214,5 @@ let functFormulario = () => {
     <span class="progress" data-value="40%"></span>
     <span class="label">40%</span>
 </div>
-</div> */}
+</div> */
+}
